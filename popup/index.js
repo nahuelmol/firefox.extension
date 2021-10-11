909d86764bbee3c45ad4c8f8cbdf9c21b97d6035
Silvio Molina <molinahuel44@gmail.com>
2021-10-11 14:57:43 -0300

    function links div, creator in template

--- popup/index.js
+++ popup/index.js
@@ -9,8 +9,43 @@ function openTab(){
 }
 
 function SearchVideos(){
+	var links = [];
+
 	document.getElementById('notify').innerHTML = "searching links";
+	console.log('There I should get links from the current analized page');
+
+	return links;
 }
+
+function CreateLinks(){
+
+	var links = SearchVideos();
+
+	for(var i=0; i < links.length; i++){
+         
+         var newDiv = document.createElement('div');
+         newDiv.id = 'r'+i;
+         newDiv.className = 'each_block';
+
+
+         var target = document.createElement('BUTTON');
+         target.id = 'btn'+i;
+         target.className = 'btns';
+         var cnt = document.createElement('div');
+         cnt.className = 'content';
+
+         var text = document.createElement('p');
+         text.className = 'txt';
+
+
+         toAdd.appendChild(newDiv);
+         newDiv.appendChild(target);
+         newDiv.appendChild(cnt);
+         cnt.appendChild(text);
+      }
+	document.getElementById('sel').appendChild(toAdd);
+}
+
 
 
 document.getElementById("OpenButton").addEventListener("click", openTab);
