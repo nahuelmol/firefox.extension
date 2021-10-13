eda5bd70b4327e02bf9615a76a24eb59ebcb1a68
Silvio Molina <molinahuel44@gmail.com>
2021-10-12 22:30:06 -0300

    Later I should resolve, how to add functionalities to the side bar. And how to play videos in the video player despite having the src setted

--- popup/player/player.js
+++ popup/player/player.js
@@ -1,9 +1,41 @@
 
+var toAdd;
+
+function MAKE(){
+
+}
+
+function CreateOptions(){
+
+	var ButtonSet = document.createElement('div')
+	ButtonSet.id = 'button_set' 
+
+	var online_sub = document.createElement('BUTTON');
+	oneline_sub.id = 'btn_sub1';
+    oneline_sub.className = 'btn_sub';
+    oneline_sub.innerHTML = 'ADD';
+    oneline_sub.onclick = MAKE;
+
+    var local_sub = document.createElement('BUTTON');
+	local_sub.id = 'btn_sub2';
+	local_sub.className = 'btn_sub';
+	local_sub.innerHTML = 'ADD';
+	local_sub.onclick = MAKE;
+
+	ButtonSet.appendChild(online_sub)
+	ButtonSet.appendChild(local_sub)
+
+	toAdd.appendChild(ButtonSet)
+}
+
 function CustomSubs(){
 	document.getElementById('custom-caption').createAttribute("src", "url.vtt")
 	document.getElementById('custom-caption').createAttribute("label", "English")
 
 	console.log("accessing to set subs");
+
+	toAdd = document.createDocumentFragment();
+    CreateOptions()
 }
 
 function SettingSrc(response){
